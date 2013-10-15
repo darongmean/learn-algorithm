d82c7c92385a259008c8ea2dc8bb03ba9bbe152d
unknown <darong012@gmail.com>
2013-10-15 14:07:42 +0700

    first commit

---
+++ test/learn_algorithm/core_test.clj
@@ -0,0 +1,7 @@
+(ns learn-algorithm.core-test
+  (:require [clojure.test :refer :all]
+            [learn-algorithm.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
